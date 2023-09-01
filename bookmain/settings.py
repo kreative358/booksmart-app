7a57e66b35397cc9c1d1f5e7c1cad3a3a99a32ce
kreative358 <kreative358@hotmail.com>
2023-09-01 12:50:15 +0200

    code

--- bookmain/settings.py
+++ bookmain/settings.py
@@ -13,7 +13,7 @@ env = environ.Env(
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-BOOKMAIN_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BOOKMAIN_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
