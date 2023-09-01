5d8db86c7f3e774f020bdb27d46a32eb88d372b3
kreative358 <kreative358@hotmail.com>
2023-09-01 14:12:07 +0200

    code

--- bookmain/settings.py
+++ bookmain/settings.py
@@ -66,7 +66,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.sites',
 
-   # "whitenoise.runserver_nostatic",
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
  
     'rest_framework',
