1ab8b2c2f9c4bfde35b51943daea8b2293c9993a
kreative358 <kreative358@hotmail.com>
2023-09-11 17:22:20 +0200

    code

--- bookmain/settings.py
+++ bookmain/settings.py
@@ -410,11 +410,12 @@ if ENVIRONMENT == 'production':
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = 'booksmartapp358@gmail.com'
-    # EMAIL_FROM = 'booksmartapp358@gmail.com'
+    EMAIL_FROM = 'booksmartapp358@gmail.com'
     EMAIL_HOST_PASSWORD = 'ikzliaiijzdvbqlq'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    DEFAULT_FROM_EMAIL = 'BookSmart App Team booksmartapp358@gmail.com'
+    DEFAULT_FROM_EMAIL = 'booksmartapp358@gmail.com'
+    SERVER_EMAIL = 'booksmartapp358@gmail.com'
 
     # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     # EMAIL_HOST = 'smtp.sendgrid.com'
