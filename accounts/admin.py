eee6ff3f6e98bf9ca96fdb6ff2e473e1f772fa95
kreative358 <kreative358@hotmail.com>
2023-11-09 15:21:03 +0100

    version 1.04

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,10 +4,10 @@ from accounts.models import Account
 
 
 class AccountAdmin(UserAdmin):
-    list_display = ('username', 'email', 'date_joined', 'last_login', 'is_admin', 'is_staff', )
+    list_display = ('id', 'username', 'email', 'date_joined', 'last_login', 'is_admin', 'is_staff', )
     search_fields = ('username', 'email',)
-    readonly_fields=('date_joined', 'last_login')
-    fields = ('password', 'username', 'email', 'date_joined', 'last_login', 'is_admin', 'is_staff', )
+    readonly_fields=('id', 'date_joined', 'last_login')
+    fields = ('id', 'password', 'username', 'email', 'date_joined', 'last_login', 'is_admin', 'is_staff', )
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
