25136fd2f1364eb511ac14edcccc3074f3bde1a4
kreative358 <kreative358@hotmail.com>
2023-11-10 11:02:58 +0100

    version 1.04

--- booksmart/scrap.py
+++ booksmart/scrap.py
@@ -1,6 +1,6 @@
 # import requests
 
-# URL = "https://realpython.github.io/fake-jobs/"
+# # URL = "https://realpython.github.io/fake-jobs/"
 # URL = "https://https://booksmart-app-bd32a8932ff0.herokuapp.com/booksmartapp/booksmart-app/all_authors/"
 # page = requests.get(URL)
 # print()
